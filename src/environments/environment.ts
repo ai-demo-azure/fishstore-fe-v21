1de1f1135f8a3e4bd8648c3213193a8d19108a0f
minhvu <123456>
2026-04-09 14:38:54 +0700

    update: backend live path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://yourdomain.com/api', // production backend
+  apiUrl: 'fishstore-web-api.azurewebsites.net', // production backend
 };
